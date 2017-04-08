f75f441c277ea57e47c701dac924a45f9f9263e2
johnfelixespinosa <johnfelix.espinosa@icloud.com>
2017-04-08 18:11:39 -0400

    started building interface

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,7 +1,31 @@
 class CLI
 
   def call
-    Scraper.new.
+    puts "Welcome to the Learn.co blog post BOT!"
+    puts "Which blog post category would you like to see?"
+    puts "
+          1. Learning to code
+          2. Career Advice
+          3. Alumni Stories
+          4. Flatiron News
+          5. Flatiron Engineering
+         "
+    puts "Enter category number:"
+
+          begin
+          user_input = gets.chomp
+          user_input = Integer(user_input)
+          rescue
+            puts "Please enter a valid category number (1-5):"
+            retry
+          end
+        
+
+    puts "#{user_input}"
+  end
+
+
+
 
 end
 
