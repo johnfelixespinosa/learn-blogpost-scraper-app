5713fb2df25bc10bfc60e89e3a966d0837705492
johnfelixespinosa <johnfelix.espinosa@icloud.com>
2017-04-30 18:03:12 -0400

    setup environment.rb

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/learn_blog_cli_app/posts'
+require_relative '../lib/learn_blog_cli_app/cli'
+require_relative '../lib/learn_blog_cli_app/scraper'
--- lib/learn_blog_cli_app.rb
+++ lib/learn_blog_cli_app.rb
@@ -1,10 +1,4 @@
-require 'open-uri'
-require 'pry'
-require 'nokogiri'
-
-require_relative './learn_blog_cli_app/posts'
-require_relative './learn_blog_cli_app/cli'
-require_relative './learn_blog_cli_app/scraper'
-
 module LearnBlogCLI
   end
+
+require_relative '../config/environment'
