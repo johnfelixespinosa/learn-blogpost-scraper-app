0bdf28c28d0e8ab0bba8b26af120759f414b7a57
johnfelixespinosa <johnfelix.espinosa@icloud.com>
2017-06-16 16:14:32 -0400

    refactored cli class to handle all interface, moved methods from the post class

--- lib/learn_blog_cli_app/cli.rb
+++ lib/learn_blog_cli_app/cli.rb
@@ -14,32 +14,31 @@ class LearnBlogCLI::CLI
          "
     puts "Enter category number:"
 
-          
     user_input = gets.chomp.to_i
 
     if user_input.between?(1, 5)
       print_category(user_input)
       print_post
     else
-      puts "Please enter number between 1-5"
+      puts "Please try again"
       call
     end
   end
 
   def print_post
+    size = LearnBlogCLI::Posts.all.size
     puts ""
     puts "Which post would you like more information on?"
-    puts "Enter post number (1-#{LearnBlogCLI::Posts.all.size}):"
+    puts "Enter post number (1-#{size})"
 
-          begin
-          post_num = gets.chomp
-          post_num = Integer(post_num)
-          rescue
-            puts "Please enter a valid post number (1-#{LearnBlogCLI::Posts.all.size}:"
-            retry
-          end
+    post_num = gets.chomp.to_i
 
-    print_post_info(post_num)
+    if post_num.between?(1, size)
+      print_post_info(post_num)
+    else
+      puts "Please try again"
+      print_post
+    end
   end
 
   def print_post_info(input)
@@ -53,8 +52,11 @@ class LearnBlogCLI::CLI
     puts "Please visit : "
     puts "#{post.url}"
     puts "For complete post"
+    puts ""
     puts "Would you like more info on another post? (Y/N)"
-    answer = gets.strip
+
+    answer = gets.strip.upcase
+
       if answer == "Y"
         LearnBlogCLI::Posts.clear
         call
@@ -64,41 +66,44 @@ class LearnBlogCLI::CLI
   end
 
   def print_category(input)
+    scrape_category = LearnBlogCLI::Scraper.new.make_category(input)
       puts ""
     if input == 1 
       puts "---------- 1. Learning to Code Blog Posts ----------"
       puts ""
-      LearnBlogCLI::Scraper.new.make_category(1)
+      scrape_category
       show_posts
     elsif input == 2
       puts "---------- 2. Career Advice Blog Posts ----------"
       puts ""
-      LearnBlogCLI::Scraper.new.make_category(2)
+      scrape_category
       show_posts
     elsif input == 3
       puts "---------- 3. Alumni Stories Blog Posts ----------"
       puts ""
-      LearnBlogCLI::Scraper.new.make_category(3)
+      scrape_category
       show_posts
     elsif input == 4
       puts "---------- 4. Flatiron News Blog Posts ----------"
       puts ""
-      LearnBlogCLI::Scraper.new.make_category(4)
+      scrape_category
       show_posts
     elsif input == 5
       puts "---------- 5. Flatiron Engineering Blog Posts ----------"
       puts ""
-      LearnBlogCLI::Scraper.new.make_category(5)
+      scrape_category
       show_posts
     end
-
-
   end
 
-    def show_posts
-      print LearnBlogCLI::Posts.list_posts
-      #binding.pry
-
+  def show_posts
+    puts ""
+    puts "---------- Current Posts ----------"
+    puts ""
+    LearnBlogCLI::Posts.all.each.with_index(1) do |posts, i|
+      puts "#{i}. #{posts.name}"
+    end
+    puts ""
   end
         
 end
--- lib/learn_blog_cli_app/posts.rb
+++ lib/learn_blog_cli_app/posts.rb
@@ -20,28 +20,6 @@ class LearnBlogCLI::Posts
     @@all.clear
   end
 
-  def self.list_posts
-    puts ""
-    puts "---------- Current Posts ----------"
-    puts ""
-    LearnBlogCLI::Posts.all.each.with_index(1) do |posts, i|
-      puts "#{i}. #{posts.name}"
-    end
-    puts ""
-  end
-
-  def self.print_post_info(input)
-    puts ""
-    puts "#{self.all[input-1].name}"
-    puts "Written by : #{self.all[input-1].author}"
-    puts ""
-    puts "#{self.all[input-1].description}..."
-    puts ""
-    puts "Please visit : "
-    puts "#{self.all[input-1].url}"
-    puts "For complete post"
-  end
-
 end 
 
 # create a post_info method that lists the remaining attributes of each variable in the all array
